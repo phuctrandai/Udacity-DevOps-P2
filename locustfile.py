5c6f74aa6dcce0ef143bbc585146f39f18715c18
PhucTD <trandaiphuc.1501@gmail.com>
2023-11-14 22:24:21 +0700

    Add locustfile.py

---
+++ locustfile.py
@@ -0,0 +1,42 @@
+from locust import HttpUser, task, between
+
+class PredictionUser(HttpUser):
+    wait_time = between(1, 3)  # Wait time between requests in seconds
+
+    @task
+    def predict(self):
+        # Define the payload for the prediction request
+        payload = {
+            "CHAS":{
+                "0":0
+            },
+            "RM":{
+                "0":6.575
+            },
+            "TAX":{
+                "0":296.0
+            },
+            "PTRATIO":{
+                "0":15.3
+            },
+            "B":{
+                "0":396.9
+            },
+            "LSTAT":{
+                "0":4.98
+            }
+        }
+
+        # Send a POST request to the prediction API endpoint
+        response = self.client.post("https://wa-udacity-devops-p2.azurewebsites.net/predict", json=payload)
+
+        # Validate the response if needed
+        if response.status_code == 200:
+            # Extract and print the prediction result
+            prediction_result = response.json()
+            print(f"Prediction Result: {prediction_result}")
+        else:
+            print(f"Error: {response.status_code}, {response.text}")
+
+# Run Locust with the following command:
+# locust -f locustfile.py
